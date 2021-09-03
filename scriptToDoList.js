0881373fed203d32115d3adc65d8dc90d86fafcd
davepcage <71347825+davepcage@users.noreply.github.com>
2021-09-02 21:21:54 -0500

    test click one

--- scriptToDoList.js
+++ scriptToDoList.js
@@ -1,5 +1,29 @@
-function homework(){
-	tarea = document.getElementById("tarea").value
-	document.getElementById("pendientes").innerHTML +=  "<div>" + tarea +"</div>" ;
-	document.getElementById("tarea").value = "";
+
+
+function ready(){
+	if (this.classList.value == "terminar") {
+		this.classList.remove("terminar");
+		this.classList.add("done");
+	}else{
+		this.classList.remove("done");
+		this.classList.add("terminar");
+	}	
 }
+
+ add = document.getElementById("add")
+ add.addEventListener("click", function(){
+	tarea = document.getElementById("job").value
+	list = document.createElement("ul")
+	element = document.createElement("li")
+	list.appendChild(element)
+	text = element.value = tarea
+	console.log(text)
+	document.getElementById("pendientes").innerHTML = tarea  ;
+	document.getElementById("job").value = "";
+
+
+	
+	// 
+	// document.getElementById("pendientes").addEventListener("click", ready());
+});
+
