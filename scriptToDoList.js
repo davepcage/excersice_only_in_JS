08523b2ebf6e236e23985a27c3bab01235d8bb6d
davepcage <71347825+davepcage@users.noreply.github.com>
2021-09-09 23:06:32 -0500

    read to do list

--- scriptToDoList.js
+++ scriptToDoList.js
@@ -1,29 +1,38 @@
 
+//  document.getElementById("add").addEventListener("click", function(){
+//   tarea = document.getElementById("job").value
+//   list = document.createElement("ul")
+//   element = document.createElement("li")
+//   list.appendChild(element)
+//   text = element.value = tarea
+//   document.getElementById("pendientes").innerHTML += "<li id='list'>" + tarea + "</li>"  ;
+//   document.getElementById("job").value = "";
+// });
 
-function ready(){
-	if (this.classList.value == "terminar") {
-		this.classList.remove("terminar");
-		this.classList.add("done");
-	}else{
-		this.classList.remove("done");
-		this.classList.add("terminar");
-	}	
+//  document.getElementById("list").addEventListener("click", function(){
+//     document.getElementById("list").classList.add("mystyle");
+//    });
+
+
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 }
-
- add = document.getElementById("add")
- add.addEventListener("click", function(){
-	tarea = document.getElementById("job").value
-	list = document.createElement("ul")
-	element = document.createElement("li")
-	list.appendChild(element)
-	text = element.value = tarea
-	console.log(text)
-	document.getElementById("pendientes").innerHTML = tarea  ;
-	document.getElementById("job").value = "";
-
-
-	
-	// 
-	// document.getElementById("pendientes").addEventListener("click", ready());
-});
-
