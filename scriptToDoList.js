7d37cc5b695cbe563649ade8803d02f3d8ddb375
davepcage <71347825+davepcage@users.noreply.github.com>
2021-09-24 12:35:41 -0500

    add list (#1)
    * add list
    
    * test click one
    
    * old to do list
    
    * -SPACE
    
    * read to do list
    
    * my code to do list
    
    * to do list complete

--- scriptToDoList.js
+++ scriptToDoList.js
@@ -1,1 +1,15 @@
-scriptToDoList.js
+var list = document.querySelector('ul');
+list.addEventListener('click', function(event) {
+  if (event.target.tagName == 'LI') {
+    event.target.classList.toggle('checked');
+  }
+}, false);
+
+document.getElementById("add").addEventListener("click", function(){
+  tarea = document.getElementById("job").value
+  element = document.createElement("li");
+  t = document.createTextNode(tarea);
+  element.appendChild(t);
+  document.getElementById("pendientes").appendChild(element);
+  document.getElementById("job").value = "";
+});
