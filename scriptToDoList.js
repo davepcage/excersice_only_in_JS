d0837b6804f761641ee5aea7f233d94b6929c294
davepcage <71347825+davepcage@users.noreply.github.com>
2021-10-12 14:14:02 -0500

    add footer

--- scriptToDoList.js
+++ scriptToDoList.js
@@ -13,4 +13,13 @@ document.getElementById("add").addEventListener("click", function(){
   document.getElementById("pendientes").appendChild(element);
   document.getElementById("job").value = "";
 });
-  
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}  
