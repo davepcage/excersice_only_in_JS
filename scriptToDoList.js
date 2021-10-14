07e65b09c9299b0056e4f1b7fa149d8b213cad6b
davepcage <71347825+davepcage@users.noreply.github.com>
2021-10-14 15:01:33 -0500

    PR comments

--- scriptToDoList.js
+++ scriptToDoList.js
@@ -13,13 +13,3 @@ document.getElementById("add").addEventListener("click", function(){
   document.getElementById("pendientes").appendChild(element);
   document.getElementById("job").value = "";
 });
-
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}  
